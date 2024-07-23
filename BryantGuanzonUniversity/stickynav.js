5cf9058c0f4fd88066b5f157bb6ba8038971e702
blguanzon <bryant.l.guanzon@gmail.com>
2024-07-24 05:02:31 +0800

    Delete BryantGuanzonUniversity directory

--- BryantGuanzonUniversity/stickynav.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = function() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.querySelector('.sticky-header').style.display = 'flex';
-    } else {
-        document.querySelector('.sticky-header').style.display = 'none';
-    }
-};
